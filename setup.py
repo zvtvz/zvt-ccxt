c568265d442ad4ea96c29d96750d1ac55c204126
foolcage <5533061@qq.com>
2020-07-17 23:28:36 +0800

    release 0.0.1

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 
 try:
@@ -7,6 +12,15 @@ try:
 except ImportError:
     # for pip <= 9.0.3
     from pip.req import parse_requirements
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
 
 install_reqs = parse_requirements("requirements.txt", session=False)
 
@@ -17,6 +31,8 @@ except:
 
 setup(
     name="zvt-ccxt",
+    version='0.0.1',
+    description='ccxt cryptocurrency plugin for zvt ',
     install_requires=requirements,
     entry_points={"zvt": ["ccxt = zvt_ccxt"]},
     py_modules=["zvt_ccxt"],
@@ -24,4 +40,32 @@ setup(
     package_data={
         'zvt_ccxt.accounts': ['*.json']
     },
+    long_description=long_description,
+    url='https://github.com/zvtvz/zvt',
+    author='foolcage',
+    author_email='5533061@qq.com',
+    classifiers=[  # Optional
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Customer Service',
+        'Intended Audience :: Education',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+
+    keywords='quant zvt cryptocurrency coin btc eos',
+
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/zvtvz/zvt-ccxt/issues',
+        'Funding': 'https://github.com/zvtvz/zvt-ccxt',
+        'Say Thanks!': 'https://saythanks.io/to/foolcage',
+        'Source': 'https://github.com/zvtvz/zvt-ccxt',
+    },
+
+    include_package_data=True,
+    long_description_content_type="text/markdown",
 )
